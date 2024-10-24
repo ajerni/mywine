1e530c71404ae2b058a4b324df06744aac492ce1
„Andi <ajerni@gmail.com>
2024-10-24 23:13:00 +0200

    main folder changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <h1>My Wine Cellar</h1>
+        <h2>A place to store your wine collection</h2>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
