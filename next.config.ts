9791320831a51c64a3570cdd1a841d9f79bf931a
„Andi <ajerni@gmail.com>
2024-10-26 21:45:37 +0200

    changes for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  // Your Next.js configuration options here
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    // Server-side configurations
-    // Add any server-side configurations here if needed
-  },
-  publicRuntimeConfig: {
-    // Client-side configurations
-    // Add any client-side configurations here if needed
-  },
-  webpack: (config: import('next').NextConfig['webpack'], { isServer }: { isServer: boolean }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer && config) {
-      (config as any).resolve = {
-        ...(config as any).resolve,
-        fallback: {
-          ...(config as any).resolve?.fallback,
-          fs: false,
-        },
-      };
-    }
-    return config;
-  },
+  swcMinify: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
