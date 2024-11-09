1d80ff5c664ca92171b0b047aa9425a0da8fa3ca
„Andi <ajerni@gmail.com>
2024-11-09 10:53:13 +0100

    92 name

--- src/app/wine-cellar/WineCellarContent.tsx
+++ src/app/wine-cellar/WineCellarContent.tsx
@@ -89,7 +89,7 @@ const MobileWineList = ({ wines, onEdit, onDelete, onRowClick }: {
         className="flex items-center pl-4 pr-1 py-3 hover:bg-gray-50 active:bg-gray-100 ios-button-alignment"
         onClick={(event) => onRowClick(event, wine)}
       >
-        <div className="flex-[7] min-w-0 pr-4">
+        <div className="flex-[7] min-w-0 pr-1">
           <div className="truncate">{wine.name}</div>
         </div>
         <div className="w-[80px] text-center">{wine.quantity}</div>
