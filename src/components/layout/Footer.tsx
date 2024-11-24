3390352df919636f7b0a155f122be236b508140a
„Andi <ajerni@gmail.com>
2024-11-24 13:09:13 +0100

    Footer black

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,10 +12,10 @@ export function Footer() {
       <div className="container mx-auto flex justify-between items-center text-red-500">
         {!hideText && (
           <>
-            <div className="footer-text">
+            <div className="footer-text text-black">
               © {new Date().getFullYear()} MyWine.info
             </div>
-            <div className="flex gap-4">
+            <div className="flex gap-4 text-black">
               <DisclaimerModal>
                 <button className="hover:text-gray-200 transition-colors footer-text">
                   Legal Disclaimer
